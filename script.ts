0aa46e3c509ad2671ba4354d9cff554e4877d1ed
itsAnanth <danonymousarmy@gmai.com>
2023-01-01 19:20:20 +0530

    build & curves

--- script.ts
+++ script.ts
@@ -5,19 +5,24 @@ const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 
+const screenRatio = Math.floor(window.innerWidth / window.innerHeight);
+const treeLength = window.innerHeight / 3;
+
 
 
 const random = {
     leafRadius: () => (Math.random() * 20) + 10,
     hex: (): string => `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
     treeAngle: (): number => (Math.random() * 20) + 5,
-    branchWidth: (): number => (Math.random() * 30) + 20,
+    branchWidth: (): number => ((Math.random() * 30) + 20) / screenRatio,
     leafLimit: (): number => Math.floor(Math.random() * 5) + 5,
-    nop: (num: number) => Math.round(Math.random()) == 1 ? -num : num
+    nop: (num: number) => Math.round(Math.random()) == 1 ? -num : num,
+    curve: (): number => Math.random() * 10
 }
 
 let randomAngle = random.treeAngle(),
-    leafLimit = random.leafLimit()
+    leafLimit = random.leafLimit(),
+    treeCurve = random.curve();
 
 function drawTree(startX: number, startY: number, length: number, angle: number, branchWidth: number, color: { branch: string, leaf: string }) {
     ctx.beginPath();
@@ -30,7 +35,13 @@ function drawTree(startX: number, startY: number, length: number, angle: number,
     ctx.translate(startX, startY);
     ctx.rotate(toRadians(angle));
     ctx.moveTo(0, 0);
-    ctx.lineTo(0, -length);
+
+    if (angle > 0) {
+        ctx.bezierCurveTo(treeCurve, -length / 2, treeCurve, -length / 2, 0, -length);
+    } else {
+        ctx.bezierCurveTo(treeCurve, -length / 2, -treeCurve, -length / 2, 0, -length);
+    }
+
     ctx.stroke();
 
     if (length < 10) {
@@ -57,6 +68,10 @@ function toRadians(degrees: number): number {
 }
 
 
-drawTree(canvas.width / 2, canvas.height, 200, 0, random.branchWidth(), { branch: random.hex(), leaf: random.hex() });
+drawTree(canvas.width / 2, canvas.height, treeLength, 0, random.branchWidth(), { branch: random.hex(), leaf: random.hex() });
 
-console.log(canvas.width)
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    drawTree(canvas.width / 2, canvas.height, canvas.height / 3, 0, random.branchWidth(), { branch: random.hex(), leaf: random.hex() });
+})
